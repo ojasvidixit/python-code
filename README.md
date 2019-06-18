c3aef9e3d77651f5fe5454cbd74f19db72f7d0ad
ojasvidixit <ojasvidixit92@gmail.com>
2019-06-18 06:34:25 +0530

    my first version of add application

---
+++ README.md
@@ -0,0 +1,3 @@
+#this is repo for python codes
+##this is code I have learned during python training
+###total python repository
---
+++ add.py
@@ -0,0 +1,8 @@
+x=int(input("enter x:"))
+y=int(input("enter y:"))
+z=x+y
+print("*"*80)
+print("x= ",x)
+print("y= ",y)
+print("z= ",z)
+print("*"*80)
